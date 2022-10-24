1f52bf564dfa869eb7fd67556d8cc7025a227175
Arslan591 <68608524+Arslan591@users.noreply.github.com>
2022-10-24 08:47:03 -0700

    Add files via upload
    Done it

---
+++ customPromise.js
@@ -0,0 +1,47 @@
+
+
+function customPromise(exc){
+         
+     const rtvalue=   exc(resolve,reject)
+     
+
+this.afterthen=(res,rej)=>{
+if(rtvalue!=''){
+    res(rtvalue)
+
+} else{
+rej(rtvalue)
+}
+}
+
+
+
+}
+
+
+function resolve(res){
+    let value =res;
+    return value;
+   
+    
+}
+
+function reject(rej){
+    let value = rej;
+    return value;
+}
+
+let mycustomPromise = new customPromise((resolve,reject)=>{
+    let x = 0;
+    if(x==0){
+      return  resolve('OK');
+    
+    }
+    else{
+      return  reject('Fail');
+    }
+    
+});
+console.log(mycustomPromise);
+
+console.log(mycustomPromise.afterthen((data)=>console.log(data),()=>console.log('error')))
